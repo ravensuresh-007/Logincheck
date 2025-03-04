284251322c0aea65b7164bd59c7deacca63b340c
ravensuresh-007 <ravensuresh@gmail.com>
2025-03-04 15:30:46 +0530

    Update New.py

--- New.py
+++ New.py
@@ -5,12 +5,14 @@ Config.read('Config.ini')
 
 # Get user configuration
 source = Config.get('user', 'source')
-client_id = "ATM011948"
-client_name = Config.get(client_id, 'client_name')
+print(source)
 
-# Extract API credentials
-API_KEY_MarketData = Config.get(client_id, 'market_data_api_key')
-API_SECRET_MarketData = Config.get(client_id, 'market_data_api_secret')
-API_KEY_Interactive = Config.get(client_id, 'interactive_api_key')
-API_SECRET_Interactive = Config.get(client_id, 'interactive_api_secret')
-print(API_KEY_MarketData)
+# client_id = "ATM011948"
+# client_name = Config.get(client_id, 'client_name')
+
+# # Extract API credentials
+# API_KEY_MarketData = Config.get(client_id, 'market_data_api_key')
+# API_SECRET_MarketData = Config.get(client_id, 'market_data_api_secret')
+# API_KEY_Interactive = Config.get(client_id, 'interactive_api_key')
+# API_SECRET_Interactive = Config.get(client_id, 'interactive_api_secret')
+# print(API_KEY_MarketData)
